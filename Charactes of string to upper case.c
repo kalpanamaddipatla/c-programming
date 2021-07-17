2a2e2bd18de67aa460ac6d6021ffc8d66387d698
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-07-17 14:23:05 +0530

    Create Charactes of string to upper case.c

---
+++ Charactes of string to upper case.c
@@ -0,0 +1,21 @@
+# c-programming
+#include <stdio.h>
+int main() 
+{
+    char str[100],upper_str[100];
+    int i=0,j=0;
+    printf("\nEnter the string:");
+    gets(str);
+    while(str[i] !='\0')
+    {
+     if(str[i]>='a'&& str[i]<='z')
+     upper_str[j]=str[i]-32;
+     else
+     upper_str[j]=str[i];
+     i++; j++;
+    }
+    upper_str[j]='\0';
+    printf("\nThe string converted into upper case is:");
+    puts(upper_str);
+   return 0;
+}
