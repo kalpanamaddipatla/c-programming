c9294affae0b5333fb9233116fcae15ae18b48b6
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-07-19 14:09:03 +0530

    Create String comparison.c

---
+++ String comparison.c
@@ -0,0 +1,13 @@
+# c-programming
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+   char str1[10]="HELLO";
+   char str2[10]="HEY";
+   if(strncmp(,str2,2)==0)
+    printf("\nThe two strings are identical");
+    else
+    printf("\n The two strings are not identical");
+    return 0;
+}
