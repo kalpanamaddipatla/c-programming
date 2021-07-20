8716ccc861cc8cb19075fa9fb688b636f912f244
20A91A04F3 <81309071+kalpanamaddipatla@users.noreply.github.com>
2021-07-20 14:05:24 +0530

    Create Stringcompare.c

---
+++ Stringcompare.c
@@ -0,0 +1,14 @@
+# c-programming
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+  char str1[]="abcd",str2[]="abcd";
+   int result;
+   result=strcmp(,str2);
+   if(result==0)
+  printf("Two strings are equals");
+  else
+  printf("Two strings are not equal");
+    return 0;
+}
